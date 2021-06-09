1a69cf8376acefb405c53f88419884899c618bd5
Felix <f.stoecker@consolinno.de>
2021-06-09 12:57:46 +0200

    Started to Add Commentary to the ExceptionalState

--- io.openems.edge.exceptionalstate.api/src/io/openems/edge/exceptionalstate/api/ExceptionalState.java
+++ io.openems.edge.exceptionalstate.api/src/io/openems/edge/exceptionalstate/api/ExceptionalState.java
@@ -8,8 +8,17 @@ import io.openems.edge.common.channel.IntegerWriteChannel;
 import io.openems.edge.common.channel.WriteChannel;
 import io.openems.edge.common.component.OpenemsComponent;
 
+/**
+ * The ExceptionalState Nature. It Provides 2 Basic Channel. The First one is the EnableSignal.
+ * That notifies the component if the enable signal is set and the Value Channel, which tells the Component what to do.
+ * Example: If a Heater receives the Enable signal and the Value is > 0 -> it activates/starts to heat, otherwise it won't heat.
+ * Ignoring all the other controller/enableSignal of the heater interface etc etc.
+ */
 public interface ExceptionalState extends OpenemsComponent {
     enum ChannelId implements io.openems.edge.common.channel.ChannelId {
+        /**
+         *
+         */
         EXCEPTIONAL_STATE_ENABLE_SIGNAL(Doc.of(OpenemsType.BOOLEAN).accessMode(AccessMode.READ_WRITE)),
         EXCEPTIONAL_STATE_VALUE(Doc.of(OpenemsType.INTEGER).accessMode(AccessMode.READ_WRITE).onInit(channel ->
                 ((IntegerWriteChannel) channel).onSetNextWrite(channel::setNextValue)));
