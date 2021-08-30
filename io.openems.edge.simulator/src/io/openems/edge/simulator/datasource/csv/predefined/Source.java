b0036077c97846b5339259b1d60f81aa9d763b4a
Paul Wimmer <info@wimmerp.de>
2021-08-30 08:59:59 +0200

    better simulation

--- io.openems.edge.simulator/src/io/openems/edge/simulator/datasource/csv/predefined/Source.java
+++ io.openems.edge.simulator/src/io/openems/edge/simulator/datasource/csv/predefined/Source.java
@@ -4,6 +4,7 @@ public enum Source {
 	ZERO("zero.csv"), //
 	H0_HOUSEHOLD_SUMMER_WEEKDAY_STANDARD_LOAD_PROFILE("h0-summer-weekday-standard-load-profile.csv"), //
 	H0_HOUSEHOLD_SUMMER_WEEKDAY_PV_PRODUCTION("h0-summer-weekday-pv-production.csv"), //
+	H0_HOUSEHOLD_SUMMER_WEEKDAY_PV_PRODUCTION_SHORT_NIGHT("h0-summer-weekday-pv-production-short-night.csv"), //
 	H0_HOUSEHOLD_SUMMER_WEEKDAY_NON_REGULATED_CONSUMPTION("h0-summer-weekday-non-regulated-consumption.csv"), //
 	H0_HOUSEHOLD_SUMMER_WEEKDAY_PV_PRODUCTION2("h0-summer-weekday-pv-production2.csv");
 
