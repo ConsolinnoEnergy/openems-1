afe5cb8b23c026a209ab62e59e567eb891b050e5
Felix <f.stoecker@consolinno.de>
2021-11-24 18:03:15 +0100

    First Implementation Pre Testing done

--- io.openems.edge.heater.cluster/src/io/openems/edge/heater/cluster/ClusteredHeaterImpl.java
+++ io.openems.edge.heater.cluster/src/io/openems/edge/heater/cluster/ClusteredHeaterImpl.java
@@ -27,6 +27,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -50,7 +51,8 @@ public class ClusteredHeaterImpl extends AbstractOpenemsComponent implements Ope
     private EnergyControlMode energyControlModeInput = EnergyControlMode.KW;
     private EnergyControlMode energyControlModeOutput = EnergyControlMode.KW;
     private float currentPowerSelected = -1.f;
-    private Map<Heater, Integer> currentRunningHeatersWithSetPoint = new HashMap<>();
+    private float previousPowerSelected = -1.f;
+    private final Map<Heater, Integer> currentRunningHeatersWithSetPoint = new HashMap<>();
     private Config config;
     private boolean configSuccess = false;
 
@@ -141,79 +143,171 @@ public class ClusteredHeaterImpl extends AbstractOpenemsComponent implements Ope
     @Override
     public void handleEvent(Event event) {
         if (this.configSuccess) {
+            if (event.getTopic().equals(EdgeEventConstants.TOPIC_CYCLE_AFTER_CONTROLLERS)) {
+                //refresh default ActivePowerChannel if externally set
+                if (this.getDefaultActivePowerChannel().getNextWriteValue().isPresent()) {
+                    this.getDefaultActivePowerChannel().setNextValue(this.getDefaultActivePowerChannel().getNextWriteValueAndReset().get());
+                }
+                //Externally Set EnableSignal
+                if (this.getEnableSignalChannel().getNextWriteValueAndReset().orElse(false)) {
+                    this.getEnableSignalChannel().setNextValue(true);
 
-            if (this.getEnableSignalChannel().getNextWriteValueAndReset().orElse(false)) {
-                this.getEnableSignalChannel().setNextValue(true);
+                    float selectedPowerLevelThisRun = -1.f;
+                    //Convert Values depending on expected INPUT -> Convert to general KW input!
+                    float defaultPower = this.convertPower((float)
+                            this.getDefaultActivePowerChannel().value()
+                                    .orElse(this.getDefaultActivePowerChannel().getNextValue().orElse(100)));
+                    Float recommendedPowerLevel = this.convertPower(this.getRecommendedSetPointHeatingPower().getNextWriteValue().orElse(null));
+                    Float overwritePowerLevel = this.convertPower(this.getOverWriteSetPointHeatingPowerChannel().getNextWriteValueAndReset().orElse(null));
+                    List<Heater> keySet = new ArrayList<>(this.currentRunningHeatersWithSetPoint.keySet());
+                    keySet.forEach(entry -> {
+                        if (entry.getHeaterState().asEnum().equals(HeaterState.BLOCKED_OR_ERROR)) {
+                            this.currentRunningHeatersWithSetPoint.remove(entry);
+                        }
+                    });
 
-                float selectedPowerLevelThisRun = -1.f;
-                //Convert Values depending on expected INPUT -> Convert to general KW input!
-                float defaultPower = this.convertPowerInput((float)
-                        this.getDefaultActivePowerChannel().value()
-                                .orElse(this.getDefaultActivePowerChannel().getNextValue().orElse(100)));
-                Float recommendedPowerLevel = this.convertPowerInput(this.getRecommendedSetPointHeatingPower().getNextWriteValue().orElse(null));
-                Float overwritePowerLevel = this.convertPowerInput(this.getOverWriteSetPointHeatingPowerChannel().getNextWriteValueAndReset().orElse(null));
-                List<Heater> keySet = new ArrayList<>(this.currentRunningHeatersWithSetPoint.keySet());
-                keySet.forEach(entry -> {
-                    if (entry.getHeaterState().asEnum().equals(HeaterState.BLOCKED_OR_ERROR)) {
-                        this.currentRunningHeatersWithSetPoint.remove(entry);
+                    //recommended is always active if overwriteIsNot
+                    if (recommendedPowerLevel != null) {
+                        selectedPowerLevelThisRun = recommendedPowerLevel;
+                        this.currentPowerSelected = recommendedPowerLevel;
                     }
-                });
+                    if (overwritePowerLevel != null) {
+                        selectedPowerLevelThisRun = Math.max(overwritePowerLevel, selectedPowerLevelThisRun);
+                    }
+                    if (recommendedPowerLevel == null && overwritePowerLevel == null) {
+                        selectedPowerLevelThisRun = defaultPower;
+                    }
 
-                //recommended is always active if overwriteIsNot
-                if (recommendedPowerLevel != null) {
-                    selectedPowerLevelThisRun = recommendedPowerLevel;
-                    this.currentPowerSelected = recommendedPowerLevel;
-                }
-                if (overwritePowerLevel != null) {
-                    selectedPowerLevelThisRun = Math.max(overwritePowerLevel, selectedPowerLevelThisRun);
-                }
-                if (recommendedPowerLevel == null && overwritePowerLevel == null) {
-                    selectedPowerLevelThisRun = defaultPower;
-                }
+                    //Now i know what percentage i need to get / what KW
+                    // This is either the Maximum of the previous run or the recommended Power
+                    this.currentPowerSelected = Math.max(selectedPowerLevelThisRun, this.currentPowerSelected);
 
-                //Now i know what percentage i need to get / what KW
-                // This is either the Maximum of the previous run or the recommended Power
-                this.currentPowerSelected = Math.max(selectedPowerLevelThisRun, this.currentPowerSelected);
+                    float currentPowerPossible = 0.f;
+                    //How much Power is possible atm!
+                    if (this.currentRunningHeatersWithSetPoint.size() > 0) {
+                        currentPowerPossible = this.currentRunningHeatersWithSetPoint.size() * this.maxIndividualHeatingPowerKw;
+                    }
+                    //Add Heater if demand cannot be supplied
+                    if (this.currentPowerSelected > currentPowerPossible) {
+                        //set HeaterSetPoint existing to Maximum
+                        this.currentRunningHeatersWithSetPoint.replaceAll((key, value) -> this.maxIndividualHeatingPowerKw);
+                        //add possible Heater
+                        this.addRunningHeatersDependingOnPowerDemand(this.currentPowerSelected, currentPowerPossible);
+                        this.previousPowerSelected = this.currentPowerSelected;
+                    } else if (this.previousPowerSelected != this.currentPowerSelected) {
+                        //Adapt since powerSupply can be met with current running heaters
+                        this.adaptPowerSupplyRunningHeater();
+                        this.previousPowerSelected = this.currentPowerSelected;
+                    }
 
-                float currentPowerPossible = 0.f;
-                //How much Power is possible atm!
-                if (this.currentRunningHeatersWithSetPoint.size() > 0) {
-                    currentPowerPossible = this.currentRunningHeatersWithSetPoint.size() * this.maxIndividualHeatingPowerKw;
-                }
-                //Add Heater if demand cannot be supplied
-                if (this.currentPowerSelected > currentPowerPossible) {
-                    //set HeaterSetPoint existing to Maximum
-                    this.currentRunningHeatersWithSetPoint.replaceAll((key, value) -> this.maxIndividualHeatingPowerKw);
-                    //add possible Heater
-                    this.addRunningHeatersDependingOnPowerDemand(this.currentPowerSelected, currentPowerPossible);
+                    this.enableSelectedRunningHeatersAndConvertPowerSetPoint();
+
+
                 } else {
-                    //Adapt since powerSupply can be met with current running heaters
-                    this.adaptPowerSupplyRunningHeater();
+                    this.getEnableSignalChannel().setNextValue(false);
+                    this.getClusterStateChannel().setNextValue(ClusterState.OFF.getValue());
+                    this.currentPowerSelected = -1.f;
                 }
-
-                this.enableSelectedRunningHeatersAndConvertPowerSetPoint();
-
-
-            } else {
-                this.getEnableSignalChannel().setNextValue(false);
-                this.getClusterStateChannel().setNextValue(ClusterState.OFF.getValue());
-                this.currentPowerSelected = -1.f;
             }
-
         } else {
             this.activationOrModificationRoutine(this.config);
         }
     }
 
+    /**
+     * PowerSetPoint is calculated -> now apply them to the heater. Remember StoredPowerSetPoint is in KW
+     */
+
     private void enableSelectedRunningHeatersAndConvertPowerSetPoint() {
 
+        this.currentRunningHeatersWithSetPoint.forEach((heater, setPoint) -> {
+            Float convertedSetPoint = this.convertPower((float) setPoint);
+            if (convertedSetPoint != null) {
+                try {
+                    heater.getEnableSignalChannel().setNextWriteValueFromObject(true);
+                    switch (this.energyControlModeOutput) {
+                        case PERCENT:
+                            heater.setHeatingPowerPercentSetpoint((convertedSetPoint * this.maxIndividualHeatingPowerKw) / 100.f);
+                            break;
+                        case KW:
+                        case TEMPERATURE:
+                        default:
+                            heater.setHeatingPowerSetpoint(convertedSetPoint);
+                            break;
+                    }
+                } catch (OpenemsError.OpenemsNamedException e) {
+                    this.log.warn("Couldn't set EnableSignal or PowerSetPoint to heater: " + heater.id()
+                            + " in : " + this.id() + " in Method enableSelectedRunningHeatersAndConvertPowerSetPoint");
+                    this.log.warn("Reason: " + e.getCause());
+                }
+            }
+            });
+
     }
 
+    /**
+     * When existing heaters are enough to provide enough power. Regulate them depending on the demand!
+     */
     private void adaptPowerSupplyRunningHeater() {
+        AtomicInteger powerToApply = new AtomicInteger((int) Math.ceil(this.currentPowerSelected));
+        this.currentRunningHeatersWithSetPoint.forEach((heater, setPoint) -> {
+            powerToApply.set(powerToApply.get() - setPoint);
+        });
+        boolean increasePerformance = powerToApply.get() > 0;
+        //Power > 0 -> increase Performance of Heater! and reduce in the process the remaining powerToApply
+        // else REDUCE
+        if (increasePerformance) {
+            Arrays.asList(ClusteredHeaterPriority.values()).forEach(priority -> {
+                Map<Integer, Heater> priorityMap = this.priorityToHeaterMap.get(priority);
+                if (priorityMap != null && !priorityMap.isEmpty() && powerToApply.get() != 0) {
+                    for (int x = 0; x < priorityMap.size(); x++) {
+                        Heater heaterToGet = priorityMap.get(x);
+                        int powerProvided = this.currentRunningHeatersWithSetPoint.get(heaterToGet);
+                        //increase Performance
+                        if (powerToApply.get() > 0) {
+                            if (powerProvided + powerToApply.get() > this.maxIndividualHeatingPowerKw) {
+                                this.currentRunningHeatersWithSetPoint.replace(heaterToGet, this.maxIndividualHeatingPowerKw);
+                                powerToApply.set(powerToApply.get() - (this.maxIndividualHeatingPowerKw - powerProvided));
+                            } else {
+                                this.currentRunningHeatersWithSetPoint.replace(heaterToGet, (powerProvided + powerToApply.get()));
+                                powerToApply.set(0);
+                            }
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            });
+        } else {
+            //Reverse order of ClusteredPriority and reduce from least priority to max priority
+            List<ClusteredHeaterPriority> values = Arrays.asList(ClusteredHeaterPriority.values());
+            Collections.reverse(values);
+            values.forEach(priority -> {
+                Map<Integer, Heater> priorityMap = this.priorityToHeaterMap.get(priority);
+                if (priorityMap != null && priorityMap.size() > 0 && powerToApply.get() != 0) {
+                    for (int x = priorityMap.size() - 1; x >= 0; x--) {
+                        Heater heaterToGet = priorityMap.get(x);
+                        int powerProvided = this.currentRunningHeatersWithSetPoint.get(heaterToGet);
+                        //if power needs to be reduced
+                        if (powerToApply.get() < 0) {
+                            if (powerProvided - powerToApply.get() <= 0) {
+                                powerToApply.set(powerToApply.get() + powerProvided);
+                                this.currentRunningHeatersWithSetPoint.remove(heaterToGet);
+                            } else {
+                                this.currentRunningHeatersWithSetPoint.replace(heaterToGet, (powerProvided + powerToApply.get()));
+                                powerToApply.set(0);
+                            }
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            });
+        }
 
     }
 
-    private Float convertPowerInput(Float inputValue) {
+    private Float convertPower(Float inputValue) {
         if (inputValue == null) {
             return null;
         } else {
@@ -238,6 +332,7 @@ public class ClusteredHeaterImpl extends AbstractOpenemsComponent implements Ope
 
     /**
      * Demand is above providing -> add new Heater to currentlyHandled Heater and adapt Power.
+     *
      * @param currentPowerSelected the maximum Power needed this time
      * @param currentPowerPossible power Possible at the moment -> this allows to calculate the powerNeeded
      */
@@ -256,6 +351,7 @@ public class ClusteredHeaterImpl extends AbstractOpenemsComponent implements Ope
             });
         });
         AtomicInteger neededHeater = new AtomicInteger(roundedNeededHeater);
+        //Calculate the Power the LastHeater in line needs
         AtomicInteger powerSetPointLastHeater = new AtomicInteger((int) Math.ceil(powerNeeded));
         if (neededHeater.get() > 1) {
             powerSetPointLastHeater.set((int) Math.ceil(powerNeeded - ((neededHeater.get()) - 1) * this.maxIndividualHeatingPowerKw));
@@ -273,9 +369,11 @@ public class ClusteredHeaterImpl extends AbstractOpenemsComponent implements Ope
                 });
             }
         });
-
+        //happens if possibleHeaterToAdd are less than needed Heater
         if (neededHeater.get() > 0) {
             this.getClusterStateChannel().setNextValue(ClusterState.NOT_ENOUGH_POWER.getValue());
+            int missingPower = (neededHeater.get() - 1) * this.maxIndividualHeatingPowerKw + powerSetPointLastHeater.get();
+            this.log.warn("Couldn't set enough Heater to provide Power. Missing Power: " + missingPower);
         } else {
             this.getClusterStateChannel().setNextValue(ClusterState.OK.getValue());
         }
