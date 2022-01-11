38ae9dc881b6f27bc6146edad308479a32bbde5d
Felix <f.stoecker@consolinno.de>
2022-01-11 10:46:32 +0100

    possible schedule fix for translator

--- io.openems.edge.controller.optimizer/src/io/openems/edge/controller/optimizer/translator/BrokerJsonTranslatorImpl.java
+++ io.openems.edge.controller.optimizer/src/io/openems/edge/controller/optimizer/translator/BrokerJsonTranslatorImpl.java
@@ -123,6 +123,7 @@ public class BrokerJsonTranslatorImpl extends AbstractOpenemsComponent implement
         if (this.configurationDone) {
             List<List<String>> newSchedule;
             newSchedule = this.createSchedule();
+            DateTime newTime = new DateTime();
             if (this.lastSchedule == null) {
                 if (newSchedule.isEmpty() == false) {
                     this.optimizer.handleNewSchedule(newSchedule);
@@ -134,7 +135,9 @@ public class BrokerJsonTranslatorImpl extends AbstractOpenemsComponent implement
 
                 //Periodically tells the Optimizer to reload the Schedule even if it is the same one.
                 //Mainly used to update after the Day changes at 00:00
-            } else if (this.now.plusMinutes(RELOAD_SCHEDULE_TIME).getMinuteOfDay() >= new DateTime().getMinuteOfDay()) {
+            } else if (this.now.plusMinutes(RELOAD_SCHEDULE_TIME).getMinuteOfDay() >= newTime.getMinuteOfDay()
+                    || this.now.getDayOfMonth() < newTime.getDayOfMonth() || this.now.getMonthOfYear() < newTime.getMonthOfYear()
+                    || this.now.getYear() < newTime.getYear()) {
                 this.optimizer.handleNewSchedule(newSchedule);
                 this.now = new DateTime();
             }
