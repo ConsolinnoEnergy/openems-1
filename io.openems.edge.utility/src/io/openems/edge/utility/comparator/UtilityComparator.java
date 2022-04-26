0c9030e06e3ca01e3ed2a1d7ac963aed4cfbecde
Felix <f.stoecker@consolinno.de>
2022-04-26 15:55:08 +0200

    packed_0.1.30-2

--- io.openems.edge.utility/src/io/openems/edge/utility/comparator/UtilityComparator.java
+++ io.openems.edge.utility/src/io/openems/edge/utility/comparator/UtilityComparator.java
@@ -115,7 +115,7 @@ public class UtilityComparator extends AbstractOpenemsComponent implements Opene
             try {
                 Double valueA = this.getValueWrapperValue(this.inputAValueWrapper, this.inputAType);
                 Double valueB = this.getValueWrapperValue(this.inputBValueWrapper, this.inputBType);
-                if (!valueA.equals(MISSING_VALUE) && !valueB.equals(MISSING_VALUE)) {
+                if (valueA != null && valueB != null && !valueA.equals(MISSING_VALUE) && !valueB.equals(MISSING_VALUE)) {
                     this.writeToOutput(StaticComparator.compare(this.comparatorType, (Double) TypeUtils.getAsType(OpenemsType.DOUBLE, valueA),
                             (Double) TypeUtils.getAsType(OpenemsType.DOUBLE, valueB)));
                 }
